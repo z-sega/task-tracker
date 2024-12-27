6dc902daba4b9ab3008317f4dc12371f9ec1d4f4
Ayo Onipe <ayosemail@gmail.com>
2024-12-27 10:54:22 -0700

    Add task management module

--- main.py
+++ main.py
@@ -1,4 +1,66 @@
-from task import main
+import argparse
+
+from task import (
+    load_database,
+    list_task,
+    add_task,
+    update_task,
+    delete_task,
+    mark_in_progress_task,
+    mark_done_task,
+    DATABASE_PATH,
+)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="A simple CLI for tracking tasks.", prog="TASK-TRACKER"
+    )
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # List all tasks (should support filters: maybe use options)
+    parser_list_all = subparsers.add_parser("list", help="List all tasks")
+    parser_list_all.add_argument("optional_status", nargs="?", help="Filter task by status")
+
+    # Add a task
+    parser_add = subparsers.add_parser("add", help="Add new task")
+    parser_add.add_argument("description", help="Description of task")
+
+    # Update a task
+    parser_update = subparsers.add_parser("update", help="Update existing task")
+    parser_update.add_argument("id", help="ID of task")
+    parser_update.add_argument("description", help="Description of task")
+
+    # Delete a task
+    parser_delete = subparsers.add_parser("delete", help="Delete task")
+    parser_delete.add_argument("id", help="ID of task")
+
+    # Mark task as done
+    parser_mark_done = subparsers.add_parser("mark_done", help="Mark task as done")
+    parser_mark_done.add_argument("id", help="ID of task")
+
+    # Mark task as in-progress
+    parser_mark_in_progress = subparsers.add_parser(
+        "mark_in_progress", help="Mark task as in-progress"
+    )
+    parser_mark_in_progress.add_argument("id", help="ID of task")
+
+    args = parser.parse_args()
+
+    if args.command == "list":
+        list_task(load_database(DATABASE_PATH), args.optional_status)
+    elif args.command == "add":
+        add_task(load_database(DATABASE_PATH), args.description)
+    elif args.command == "update":
+        update_task(load_database(DATABASE_PATH), args.id, args.description)
+    elif args.command == "delete":
+        delete_task(load_database(DATABASE_PATH), args.id)
+    elif args.command == "mark_in_progress":
+        mark_in_progress_task(load_database(DATABASE_PATH), args.id)
+    elif args.command == "mark_done":
+        mark_done_task(load_database(DATABASE_PATH), args.id)
+    else:
+        parser.print_help()
 
 
 if __name__ == "__main__":
--- task.py
+++ task.py
@@ -1,4 +1,8 @@
 from typing import Literal, Callable
+from datetime import datetime
+import json
+
+DATABASE_PATH = "data.json"
 
 
 def main() -> None:
@@ -8,12 +12,19 @@ def main() -> None:
 # database management
 def load_database(path: str) -> dict[str, dict]:
     "Load database from PATH."
-    pass
+    try:
+        with open(path, "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        data = {}
+
+    return data
 
 
 def save_database(database: dict[str, dict], path: str) -> None:
     "Save DATABASE to PATH."
-    pass
+    with open(path, "w") as file:
+        json.dump(database, file, indent=4)
 
 
 # query management
@@ -29,7 +40,23 @@ def get_querie(supported_queries: dict[str, dict]) -> tuple[Callable, dict]:
 # task management
 def add_task(database: dict[str, dict], description: str) -> None:
     "Add task DESCRIPTION to DATABASE."
-    pass
+
+    def new_task(id):
+        now = datetime.now().isoformat()
+        return {
+            "id": id,
+            "created": now,
+            "updated": now,
+            "status": "todo",
+            "description": description,
+        }
+
+    if database:
+        next_id = len(database) + 1
+        save_database(database.update({f"{next_id}": new_task(next_id)}), DATABASE_PATH)
+    else:
+        next_id = 1
+        save_database({}.update({f"{next_id}": new_task(next_id)}), DATABASE_PATH)
 
 
 def delete_task(database: dict[str, dict], id: str) -> None:
@@ -47,7 +74,15 @@ def list_task(
     status: Literal["all", "done", "in-progress", "todo"] = "all",
 ) -> None:
     "List tasks in DATABASE matching STATUS."
-    pass
+    match status:
+        case "done":
+            print("list-done")
+        case "in-progress":
+            print("list-in-progress")
+        case "todo":
+            print("list-todo")
+        case _:
+            print("list-all")
 
 
 def mark_in_progress_task(database: dict[str, dict], id: str) -> None:
